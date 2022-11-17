0294b897df9d9ee86d2b0158ef9c21052a55ccc6
fromherotodiaro <fromherotodiaro@gmail.com>
2022-11-17 20:33:04 +0700

    add file properties

--- DemoAppMVC/src/main/java/com/centurion/configs/WebAplicationContextConfig.java
+++ DemoAppMVC/src/main/java/com/centurion/configs/WebAplicationContextConfig.java
@@ -1,8 +1,10 @@
 package com.centurion.configs;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +13,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
-@ComponentScan({ "com.centurion" })
+@ComponentScan({ "com.centurion.controller" })
 public class WebAplicationContextConfig implements WebMvcConfigurer {
 
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
@@ -25,6 +27,15 @@ public class WebAplicationContextConfig implements WebMvcConfigurer {
 		resolver.setPrefix("/WEB-INF/jsp/");
 		resolver.setSuffix(".jsp");
 		return resolver;
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasename("messages");
+		return source;
+		
 	}
 
 }
